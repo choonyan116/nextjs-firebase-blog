1d388e943ece36c901e5fd015d0278b5adf12df2
choonyan116 <ngmouren116@gmail.com>
2021-08-13 10:04:58 +0800

    Showing test data on Homepage

---
+++ pages/create.js
@@ -0,0 +1,9 @@
+import styles from'./create.module.scss';
+
+const CreatePage = () => (
+  <div styles={className.CreatePage}>
+    <h1>Hello, from CreatePage!</h1>
+  </div>
+);
+
+export default CreatePage;
