b9c33c1e81b90de02b9c9d4745de50137b158546
choonyan116 <ngmouren116@gmail.com>
2021-08-14 14:27:51 +0800

    Adding sign out button to layout

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -1,17 +1,25 @@
+import { signOut } from '@lib/firebase';
+import { useAuth } from '@contexts/auth';
 import styles from './Layout.module.scss';
 
+const Layout = ({ children }) => {
+  const [user] = useAuth();
 
-
-const Layout = ({ children }) => (
+  return (
     <div className={styles.Layout}>
       <nav>
         <span>
           <a href="/">My Next.js Blog</a>
         </span>
+        {user && (
+          <span>
+            <button onClick={() => signOut()}>Sign Out</button>
+          </span>
+        )}
       </nav>
       <main>{children}</main>
     </div>
   );
-  
+  }
 
 export default Layout;
