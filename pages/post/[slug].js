0adea1a57c8a0cd3ae00e3911eb1a32fc7b2004a
choonyan116 <ngmouren116@gmail.com>
2021-08-14 12:57:27 +0800

    Adding new Layout component

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,17 +1,23 @@
 import { useRouter } from 'next/router';
 import { getFormattedDate } from '@lib/utils';
-import styles from '@styles/post.module.scss';
 import { getPostBySlug } from '@lib/firebase';
+import { Layout } from '@components';
+import styles from '@styles/post.module.scss';
 
 /* PostPage accept post prop and render the post */
-const PostPage = ({ post }) => (
-    <div className={styles.PostPage}>
-      <img src={post.coverImage} alt={post.coverImageAlt} />
-      <h1>{post.title}</h1>
-      <span>Published {getFormattedDate(post.dateCreated)}</span>
-      <p dangerouslySetInnerHTML={{ __html: post.content }}></p>
-    </div>
+const PostPage = ({ post }) => {
+
+return (
+    <Layout>
+      <div className={styles.PostPage}>
+        <img src={post.coverImage} alt={post.coverImageAlt} />
+        <h1>{post.title}</h1>
+        <span>Published {getFormattedDate(post.dateCreated)}</span>
+        <p dangerouslySetInnerHTML={{ __html: post.content }}></p>
+      </div>
+    </Layout>
   );
+}  
   
 
 export async function getServerSideProps(context) {
