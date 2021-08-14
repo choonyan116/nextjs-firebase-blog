accba59fab5acabf2bc381233cc856b3ba9daccd
choonyan116 <ngmouren116@gmail.com>
2021-08-14 14:47:27 +0800

    Adding edit link

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,13 +1,16 @@
 import { useRouter } from 'next/router';
 import { getFormattedDate } from '@lib/utils';
 import { getPostBySlug } from '@lib/firebase';
-import { Layout } from '@components';
+import { Icon, Layout } from '@components';
+import { useAuth } from '@contexts/auth';
 import styles from '@styles/post.module.scss';
 
 /* PostPage accept post prop and render the post */
 const PostPage = ({ post }) => {
   //Ready to route user to other pages.
   const router = useRouter();
+  const [user] = useAuth();
+
  
   /*Direct user to 404 page if post does not exists*/
   if (!post && typeof window !== 'undefined') {
@@ -22,6 +25,14 @@ const PostPage = ({ post }) => {
 return (
     <Layout>
       <div className={styles.PostPage}>
+        <div>
+          <h1>{post.title}</h1>
+          {user && (
+            <a href={`/edit/${post.slug}`}>
+              <Icon name="pencil-alt" />
+            </a>
+          )}
+        </div>
         <img src={post.coverImage} alt={post.coverImageAlt} />
         <h1>{post.title}</h1>
         <span>Published {getFormattedDate(post.dateCreated)}</span>
