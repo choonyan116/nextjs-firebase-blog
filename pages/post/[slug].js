130f1c2752e4c2c497ce3555e3a7e771563aa655
choonyan116 <ngmouren116@gmail.com>
2021-08-14 14:59:51 +0800

    Adding delete button

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,8 +1,8 @@
 import { useRouter } from 'next/router';
 import { getFormattedDate } from '@lib/utils';
-import { getPostBySlug } from '@lib/firebase';
 import { Icon, Layout } from '@components';
 import { useAuth } from '@contexts/auth';
+import { deletePost, getPostBySlug } from '@lib/firebase';
 import styles from '@styles/post.module.scss';
 
 /* PostPage accept post prop and render the post */
@@ -25,16 +25,31 @@ const PostPage = ({ post }) => {
 return (
     <Layout>
       <div className={styles.PostPage}>
+        <img src={post.coverImage} alt={post.coverImageAlt} />
         <div>
           <h1>{post.title}</h1>
           {user && (
-            <a href={`/edit/${post.slug}`}>
-              <Icon name="pencil-alt" />
-            </a>
+            <span>
+              <a href={`/edit/${post.slug}`}>
+                <Icon name="pencil-alt" />
+              </a>
+              <button
+                onClick={() => {
+                  const shouldDeletePost = confirm(
+                    'Are you sure you want to delete this post?',
+                  );
+                  if (shouldDeletePost) {
+                    deletePost(post.slug).then(() => {
+                      router.push('/');
+                    });
+                  }
+                }}
+              >
+                <Icon name="trash-alt" />
+              </button>
+            </span>
           )}
         </div>
-        <img src={post.coverImage} alt={post.coverImageAlt} />
-        <h1>{post.title}</h1>
         <span>Published {getFormattedDate(post.dateCreated)}</span>
         <p dangerouslySetInnerHTML={{ __html: post.content }}></p>
       </div>
