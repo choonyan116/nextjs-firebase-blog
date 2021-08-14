d62afe6fc629318cc333d152feb05d9cb0c265fe
choonyan116 <ngmouren116@gmail.com>
2021-08-14 11:01:15 +0800

    Rendering posts in PostPage

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,15 +1,28 @@
 import { useRouter } from 'next/router';
+import { getFormattedDate } from '@lib/utils';
 import styles from '@styles/post.module.scss';
+import { getPostBySlug } from '@lib/firebase';
 
-const PostPage = () => {
-  const router = useRouter();
-  const { slug } = router.query;
-
-  return (
+/* PostPage accept post prop and render the post */
+const PostPage = ({ post }) => (
     <div className={styles.PostPage}>
-      <h1>Hello, from post: {slug}!</h1>
+      <img src={post.coverImage} alt={post.coverImageAlt} />
+      <h1>{post.title}</h1>
+      <span>Published {getFormattedDate(post.dateCreated)}</span>
+      <p dangerouslySetInnerHTML={{ __html: post.content }}></p>
     </div>
   );
-};
+  
+
+export async function getServerSideProps(context) {
+    const post = await getPostBySlug(context.query.slug);
+  
+    return {
+      props: {
+        post,
+      },
+    };
+  }
+  
 
 export default PostPage;
